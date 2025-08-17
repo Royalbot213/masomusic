ea289f32b702a75b1778c759e85c3f6532c863d3
Royalbot213 <143687906+Royalbot213@users.noreply.github.com>
2025-08-17 10:05:43 +0530

    Update start.py

--- AviaxMusic/utils/inline/start.py
+++ AviaxMusic/utils/inline/start.py
@@ -31,8 +31,9 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.https://t.me/+64JuwiCU-EhlNDI1),
+            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/+64JuwiCU-EhlNDI1"),
         ],
     ]
     return buttons
+
 
